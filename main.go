365caa1bf42c4a3d27cba8757802548f9be4b184
mohakin <wsopeichhorn@yahoo.com>
2025-05-02 22:36:46 -0700

    setup window

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Application struct {
+	App *fyne.App
+	Win fyne.Window
+}
+
+func main() {
+	myApp := app.New()
+
+	win := myApp.NewWindow("Polyglot")
+
+	win.SetContent(widget.NewLabel("Hello World"))
+	win.SetFullScreen(true)
+	win.SetFixedSize(false)
+	win.ShowAndRun()
+}
