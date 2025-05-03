f8f39c4845932e126d77086467f1ebc7f7e44799
mohakin <wsopeichhorn@yahoo.com>
2025-05-02 22:55:57 -0700

    set defualt window size with resizing options

--- main.go
+++ main.go
@@ -6,18 +6,13 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-type Application struct {
-	App *fyne.App
-	Win fyne.Window
-}
-
 func main() {
 	myApp := app.New()
 
 	win := myApp.NewWindow("Polyglot")
 
 	win.SetContent(widget.NewLabel("Hello World"))
-	win.SetFullScreen(true)
+	win.Resize(fyne.NewSize(800, 600))
 	win.SetFixedSize(false)
 	win.ShowAndRun()
 }
